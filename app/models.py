2a25e2a59787da62b669ed524f9a108909b8ccd0
whimo <me@whimo.me>
2017-06-27 12:25:28 +0300

    Request and login methods for account model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
-from app import db
+from app import app, db
+import requests
+from bs4 import BeautifulSoup
 
 
 class User(db.Model):
@@ -31,8 +33,59 @@ class User(db.Model):
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    server_url = db.Column(db.String(16))
+    server_url = db.Column(db.String(30))
     username = db.Column(db.String(100))
     password = db.Column(db.String(100))
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    session = requests.Session()
+
+    def request(self, url, data={}):
+        try:
+            if len(data) == 0:
+                response = self.session.get(url, headers=app.config['DEFAULT_HEADERS'])
+            else:
+                response = self.session.post(url, headers=app.config['DEFAULT_HEADERS'], data=data)
+
+        except Exception:
+            print('[ERROR] Network problem, the URL {} cannot be fetched.'.format(url))
+            return False
+
+        if 'window._player_uuid = \'\'' in response.text and 'login.php' not in url and len(url) >= 22:
+            print('[WARNING] Player {} suddenly logged off, trying to relogin.'.format(self.username))
+
+            if not self.login():
+                return False
+            response = self.request(url=url, data=data)
+
+        return response
+
+    def login(self):
+        page = self.request(self.server_url + app.config['LOGIN_URL'])
+        if not page:
+            print('[ERROR] Could not get the login page for {}, login failed.'.format(self.username))
+            return False
+
+        parser = BeautifulSoup(page, 'html5lib')
+
+        s1 = parser.find('button', {'name': 's1'})['value'].encode('utf-8')
+        login_code = parser.find('input', {'name': 'login'})['value']
+
+        login_data = {
+            'name': self.username,
+            'password': self.password,
+            's1': s1,
+            'w': '1366:768',
+            'login': login_code}
+
+        page = self.request(self.server_url + app.config['LOGIN_POST_URL'], login_data)
+        if not page:
+            print('[ERROR] Could not post login data for {}, login failed.'.format(self.username))
+            return False
+
+        if 'window._player_uuid = \'\'' in page.text:
+            print('[ERROR] Could not log in player {}, probably incorrect account data provided.'.format(self.username))
+            return False
+
+        return True
--- config.py
+++ config.py
@@ -15,3 +15,19 @@ SECRET_KEY = '?iu_V-8[InY2vo@ZqHD8|nZuFTu&0{'
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dbadmin:security1338@trabobdb.corzsedgktxs.us-west-2.rds.amazonaws.com:3306/trabobdb'
 
 TELEGRAM_BOT_TOKEN = '356224566:AAEvmWALrQkLRmKhZ-Fs3AY2dqd4wLMkj80'
+
+DEFAULT_HEADERS = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                   'Accept-Encoding': 'gzip, deflate',
+                   'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+                   'Cache-Control': 'max-age=0',
+                   'Connection': 'keep-alive',
+                   'Content-Length': '96',
+                   'Content-Type': 'application/x-www-form-urlencoded',
+                   'Cookie': 'sess_id=0948a947332fade30ff074a6797ea97c; lowRes=0; T3E=Wgp51I5rBTD8iz513h7z%2Bw%3D%3D%3A1KW%2FuiNX1B9KEE0iNWPyPVbEBsqWO8Lf%2BrefgefHSkbQWIuEjnWCsGmqWXRlvJL8XVczabds4LNsm1SlCGm1%2B4OR4TYsx4%2F9McDKSiXrBM2Zs7VIoRhZrJdK7ahhdifu%3AJ%2FJrhb9DzSSIlhSyYtZByvgzS84ngggKGwFtuqa1u8Y%3D',
+                   'Host': 'ts6.travian.ru',
+                   'Upgrade-Insecure-Requests': '1',
+                   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+                   'X-Compress': 'null'}
+
+LOGIN_URL = '/login.php'
+LOGIN_POST_URL = '/dorf1.php'
